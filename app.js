342eae5beb270e4fc28580ec3b6844874e8c361b
Bea Avis <bea.avis.p@gmail.com>
2021-06-11 10:55:48 +0200

    Initial commit with basic configs

---
+++ app.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+//DB Configuration
+require('./configs/db.config');
+
+const app = express();
+
+// Middleware Setup
+require('./configs/middleware.config')(app);
+require('./configs/cors.config')(app);
+require('./configs/session.config')(app);
+require('./configs/passport.config')(app);
+
+// Catch 404 and respond with error message
+app.use((req, res, next) => {
+  return res.status(404).json({ message: 'Not found' });
+});
+
+module.exports = app;
