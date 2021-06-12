61c898e8142e4c0140c0d0c4f0c5066f72128423
Bea Avis <bea.avis.p@gmail.com>
2021-06-12 10:54:06 +0200

    Update app.js router including auth and customer routes

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ require('./configs/cors.config')(app);
 require('./configs/session.config')(app);
 require('./configs/passport.config')(app);
 
+const authRoutes = require('./routes/authRoutes');
+const customerRoutes = require('./routes/customerRoutes');
+
+app.use('/api/auth', authRoutes);
+app.use('api/customers', customerRoutes);
+
+// app.use('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public/index.html'));
+// });
+
 // Catch 404 and respond with error message
 app.use((req, res, next) => {
   return res.status(404).json({ message: 'Not found' });
