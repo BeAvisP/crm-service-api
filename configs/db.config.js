e7a5429953f8f5696d9bbbbd71de8f7503b39abf
Bea Avis <bea.avis.p@gmail.com>
2021-06-12 12:35:23 +0200

    Update db config to remove mongoose warning

--- configs/db.config.js
+++ configs/db.config.js
@@ -4,6 +4,7 @@ mongoose
   .connect(`${process.env.DB_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(() => console.log('Connected to Mongo!'))
   .catch((error) => console.error(error));
