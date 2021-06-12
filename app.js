650c31043760b06c778dee7a161a260dafc765ea
Bea Avis <bea.avis.p@gmail.com>
2021-06-12 12:36:12 +0200

    Format app.js for better structure

--- app.js
+++ app.js
@@ -13,19 +13,14 @@ require('./configs/cors.config')(app);
 require('./configs/session.config')(app);
 require('./configs/passport.config')(app);
 
+// Routes
 const authRoutes = require('./routes/authRoutes');
 const customerRoutes = require('./routes/customerRoutes');
 
 app.use('/api/auth', authRoutes);
-app.use('api/customers', customerRoutes);
-
-// app.use('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/index.html'));
-// });
+app.use('/api/customers', customerRoutes);
 
 // Catch 404 and respond with error message
-app.use((req, res, next) => {
-  return res.status(404).json({ message: 'Not found' });
-});
+app.use((req, res, next) => res.status(404).json({ message: 'Not found' }));
 
 module.exports = app;
